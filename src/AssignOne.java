1786274a1edc91ed04381f3370a7165d59ef4109
VrushaliB25 <vrbhoite@gmail.com>
2017-03-25 22:29:28 +0530

    Core Java Session 1 Assignment 1
    Example Assignment 1

---
+++ src/AssignOne.java
@@ -0,0 +1,16 @@
+
+public class AssignOne {
+	public static void main(String s[])
+	{
+		//To write a program to print the sum of two numbers without using + operator. Other operators have
+		//to be used.
+		int num1=80,num2=90,sum;
+		sum=num1-(-num2);
+		System.out.println("The sum of  "+ num1+" and "+ num2+" is "+sum);
+				
+		
+		
+		
+	}
+
+}
